4a85ef0f3ec0e1567a80087df2c8de8e432feb6b
keiino <ineedayy0425@gmail.com>
2022-01-29 06:49:28 +0900

    html of user account and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'users/profile', to: 'users/sessions#profile'
+    get 'users/account', to: 'users/sessions#account'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
